3532db3c8793e94ac58cae4e6e71443c7d01b81b
Irinka96 <iraru9683@gmail.com>
2022-07-17 22:57:59 +0300

    решение уравнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  const discriminant = b**2-4*a*c;
+  if (discriminant < 0 ) {
+    arr = []
+  }
+  else if (discriminant == 0) {
+    const result = -b/(2*a)
+    arr = [result]
+  }
+  else if (discriminant > 0) {
+    const result1 = (-b + Math.sqrt(discriminant) )/(2*a)
+    const result2 = (-b - Math.sqrt(discriminant) )/(2*a)
+    arr = [result1, result2]  
+  }
   return arr; // array
 }
 
